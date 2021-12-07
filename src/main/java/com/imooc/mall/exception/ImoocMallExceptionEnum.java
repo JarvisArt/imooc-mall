6354eca9cd1b42fd6f9dc50f3e1e0110e9ffa05b
JarvisArt <1120886013@qq.com>
2021-12-07 20:08:24 +0800

    feat: 注册接口开发、自定义异常类

--- src/main/java/com/imooc/mall/exception/ImoocMallExceptionEnum.java
+++ src/main/java/com/imooc/mall/exception/ImoocMallExceptionEnum.java
@@ -4,7 +4,11 @@ package com.imooc.mall.exception;
  * 描述：异常枚举
  */
 public enum ImoocMallExceptionEnum {
-    NEED_USER_NAME(10001, "用户名不能为空");
+    NEED_USER_NAME(10001, "用户名不能为空"),
+    NEED_PASSWORD(10002, "密码不能为空"),
+    PASSWORD_TOO_SHORT(1003, "密码长度不能少于8位"),
+    NAME_EXISTED(1004, "不予许重名，注册失败"),
+    INSERT_FAILED(1005, "插入失败，请重试");
     /**
      * 异常码
      */
--- src/main/java/com/imooc/mall/services/UserService.java
+++ src/main/java/com/imooc/mall/services/UserService.java
@@ -1,7 +1,13 @@
 package com.imooc.mall.services;
 
+import com.imooc.mall.exception.ImoocMallException;
 import com.imooc.mall.model.pojo.User;
 
+/**
+ * 描述：UserService
+ */
 public interface UserService {
     User getUser();
+
+    void register(String userName, String password) throws ImoocMallException;
 }
