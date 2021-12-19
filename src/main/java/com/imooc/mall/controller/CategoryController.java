036f8701fae325f286b7aec8204ea4fee31e2331
Jarvis <1120886013@qq.com>
2021-12-19 23:14:03 +0800

    feat: 自动生成Swagger文档，新增、更新、删除、获取列表目录接口

---
+++ src/main/java/com/imooc/mall/controller/CategoryController.java
@@ -0,0 +1,99 @@
+package com.imooc.mall.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.mall.common.ApiRestResponse;
+import com.imooc.mall.common.Constant;
+import com.imooc.mall.exception.ImoocMallExceptionEnum;
+import com.imooc.mall.model.pojo.Category;
+import com.imooc.mall.model.pojo.User;
+import com.imooc.mall.model.request.AddCategoryReq;
+import com.imooc.mall.model.request.UpdateCategoryReq;
+import com.imooc.mall.services.CategoryService;
+import com.imooc.mall.services.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+/**
+ * 描述：目录分类
+ */
+@Controller
+public class CategoryController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    /**
+     * 后台添加目录分类
+     */
+    @ApiOperation("后台添加目录分类")
+    @PostMapping("/admin/category/add")
+    @ResponseBody
+    public ApiRestResponse addCategory(HttpSession session, @Valid @RequestBody AddCategoryReq addCategoryReq) {
+
+        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
+        if (currentUser == null) {
+            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
+        }
+        // 校验是否是管理员
+        boolean adminRole = userService.checkAdminRole(currentUser);
+        if (adminRole) {
+            // 是管理员，执行操作
+            categoryService.add(addCategoryReq);
+            return ApiRestResponse.success();
+        } else {
+            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
+        }
+    }
+
+    /**
+     * 后台更新目录分类
+     */
+    @ApiOperation("后台更新目录分类")
+    @PostMapping("/admin/category/update")
+    @ResponseBody
+    public ApiRestResponse updateCategory(HttpSession session, @Valid @RequestBody UpdateCategoryReq updateCategoryReq) {
+
+        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
+        if (currentUser == null) {
+            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
+        }
+        // 校验是否是管理员
+        boolean adminRole = userService.checkAdminRole(currentUser);
+        if (adminRole) {
+            // 是管理员，执行操作
+            Category category = new Category();
+            BeanUtils.copyProperties(updateCategoryReq, category);
+            categoryService.update(category);
+            return ApiRestResponse.success();
+        } else {
+            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
+        }
+    }
+
+    @ApiOperation("后台删除目录")
+    @PostMapping("/admin/category/delete")
+    @ResponseBody
+    public ApiRestResponse deleteCategory(@RequestParam Integer id) {
+        categoryService.delete(id);
+        return ApiRestResponse.success();
+    }
+
+    @ApiOperation("后台目录列表")
+    @PostMapping("/admin/category/list")
+    @ResponseBody
+    public ApiRestResponse listCategoryForAdmin(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        PageInfo pageInfo = categoryService.listForAdmin(pageNum, pageSize);
+        return ApiRestResponse.success(pageInfo);
+    }
+}
---
+++ src/main/java/com/imooc/mall/services/CategoryService.java
@@ -0,0 +1,18 @@
+package com.imooc.mall.services;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.mall.model.pojo.Category;
+import com.imooc.mall.model.request.AddCategoryReq;
+
+/**
+ * 描述：目录分类Service
+ */
+public interface CategoryService {
+    void add(AddCategoryReq addCategoryReq);
+
+    void update(Category updateCategory);
+
+    void delete(Integer id);
+
+    PageInfo listForAdmin(Integer pageNum, Integer pageSize);
+}
